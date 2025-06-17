c432073397886666ca30ca7abd189fd2f8308cd6
justLexon <otherslexon@gmail.com>
2025-06-17 13:14:33 -0500

    footer v1

--- apps/web/components/Footer.tsx
+++ apps/web/components/Footer.tsx
@@ -7,23 +7,27 @@ import styles from './Footer.module.css';
 export default function Header() {
 
   return (
-    <header className={styles.headerMain}>
-      <div className={styles.headerLeft}>
-        <img src="/myranslogo.png" className="myronsLogo" alt="logo" />
+    <header className={styles.footerMain}>
+      <div className={styles.footerCont}>
+
+        <div className={styles.footerLeft}>
+          <img src="/myranslogo.png" className="myronsLogo" alt="logo" />
+        </div>
+
+        <nav className={styles.footerCenter}>
+          <a href="#">Lexon</a>
+          <a href="#">All Rights Reserved.</a>
+           <div className={styles.scheduleGap}>
+              <a>Mon-Sun</a>
+              <a>7:00 A.M. - 8:30 P.M.</a>
+            </div>
+        </nav>
+
+        <div className={styles.footerRight}>
+          <a>Order Now!</a>
+        </div>
+
       </div>
-
-      <nav className="headerNav">
-        <a href="#">Lexon</a>
-        <a href="#">Weekly Ad</a>
-        <a href="#">Contact</a>
-        <a href="#">About Us</a>
-      </nav>
-
-      <div className="headerRight">
-        <a>Order Now!</a>
-        <a>123-123-4567</a>
-      </div>
-
     </header>
   );
 }
