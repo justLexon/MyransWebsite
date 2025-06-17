f8c9eb83f1eb5dbe92e69d66d65304910deb3a21
justLexon <otherslexon@gmail.com>
2025-06-17 11:05:30 -0500

    v1 section1 for homepage(backup)

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -9,24 +9,54 @@ export default function Home() {
 
       <main>
         <section className={homeStyles.sectionContBehind}>
-          <div className={homeStyles.sectionCont1}> 
-            <div className={homeStyles.no}>
-              <div className={homeStyles.sect1Head}>
-                <Image 
-                  src="/myranslogo.png" 
-                  alt="logo" 
-                  className={homeStyles.headerT}
-                  width={530}
-                  height={250}
-                />
-                <a className={homeStyles.paraT}>A Southern, Savory Experience</a>
+            <div className={homeStyles.sectionCont1}> 
+              <div className={homeStyles.no}>
+                <div className={homeStyles.sect1Head}>
+                  <Image 
+                    src="/myranslogo.png" 
+                    alt="logo" 
+                    className={homeStyles.headerT}
+                    width={530}
+                    height={250}
+                  />
+                  <a className={homeStyles.paraT}>A Southern, Savory Experience</a>
+                </div>
+              </div>
+
+              <div className={homeStyles.sect1Desc}>
+
+                <div className={homeStyles.imgCont}>
+                  <div className={homeStyles.img1Cont}>
+                    <img src="cajuncrawfish.png"></img>
+                  </div>
+
+                  <div className={homeStyles.text1Cont}>
+                    <a>sum cruwfish cuh</a>
+                  </div>
+                </div>
+
+                <div className={homeStyles.imgCont}>
+                  <div className={homeStyles.img1Cont}>
+                    <img src="cajunfries.png"></img>
+                  </div>
+
+                  <div className={homeStyles.text1Cont}>
+                    <a>sum fries cuh</a>
+                  </div>
+                </div>
+
+                <div className={homeStyles.imgCont}>
+                  <div className={homeStyles.img1Cont}>
+                    <img src="cajuntenders.png"></img>
+                  </div>
+
+                  <div className={homeStyles.text1Cont}>
+                    <a>sum tenders cuh</a>
+                  </div>
+                </div>
+
               </div>
             </div>
-
-            <div className={homeStyles.sect1Desc}>
-              <a>a</a>
-            </div>
-          </div>
         </section>
 
         <section className={homeStyles.sectionCont2}>
